7d09e7da6fadea23b29a3916134c88b7a2d8a9a7
fominykhartur <fominykhartur+GitHub@gmail.com>
2021-11-22 21:58:21 +0300

    Добавлено общее меню для кнопок экспорта Добавлена возможность экспорта ворда

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { useState, useEffect } from 'react'
+import { Collapse } from 'bootstrap';
 const fs = require('fs');
 
 export default props => {
-    return (
+    var [toggle, setToggle] = useState(false);
+    
+    useEffect(() => {
+        var myCollapse = document.getElementById('collapseTarget')
+        var bsCollapse = new Collapse(myCollapse, {toggle: false})
+        toggle ? bsCollapse.show() : bsCollapse.hide()
+    })
+
+  return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <form className="form-inline">
-            <a href={`${props.API_URL}exportBooks`}><button className="btn btn-outline-success" type="button" onClick={() => console.log('База экспортирована')}>Экспорт базы</button></a>
-          </form>
+          <div className="py-2">
+              <button className="btn btn-outline-success" onClick={() => setToggle(toggle => !toggle)}>
+                  Экспорт базы
+              </button>
+              <div className="collapse py-2" id="collapseTarget">
+                  <a href={`${props.API_URL}exportExcel`}><button className="btn btn-outline-success " type="button" onClick={() => console.log('База экспортирована')}>Excel</button></a>
+                  <a href={`${props.API_URL}exportWord`}><button className="btn btn-outline-primary " type="button" onClick={() => console.log('База экспортирована')}>Word</button></a>
+              </div>
+          </div>
         </nav>
-    )
+  )
 }
