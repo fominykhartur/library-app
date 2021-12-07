106827e841073fa302a75ee737bcbd504894b70c
fominykhartur <fominykhartur+GitHub@gmail.com>
2021-12-07 17:01:48 +0300

    Переработана кнопка импорта

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import { useState, useEffect } from 'react'
 import { Collapse } from 'bootstrap';
-const fs = require('fs');
+import { Navbar, Nav, Row, Col, NavDropdown, Container, Button, ButtonGroup } from 'react-bootstrap';
 
 export default props => {
-    var [toggle, setToggle] = useState(false);
+    const [showExport, setShowExport] = useState(false);
+
+
+    var [toggle, setToggle] = useState({toggle: false, elemID: 'Export'});
+    var {elemID, toggle} = toggle;
+    console.log(elemID, toggle)
     
     useEffect(() => {
-        var myCollapse = document.getElementById('collapseTarget')
-        var bsCollapse = new Collapse(myCollapse, {toggle: false})
+        var myCollapse = document.getElementById(elemID)
+        var bsCollapse = new Collapse(myCollapse, {toggle : false})
         toggle ? bsCollapse.show() : bsCollapse.hide()
     })
 
   return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="py-2">
-              <button className="btn btn-outline-success" onClick={() => setToggle(toggle => !toggle)}>
+              <button className="btn btn-outline-success" onClick={() => setToggle({toggle:!toggle, elemID: 'Export'})}>
                   Экспорт базы
               </button>
-              <div className="collapse py-2" id="collapseTarget">
+              <div className="collapse py-2" id="Export">
                   <a href={`${props.API_URL}exportExcel`}><button className="btn btn-outline-success " type="button" onClick={() => console.log('База экспортирована')}>Excel</button></a>
                   <a href={`${props.API_URL}exportWord`}><button className="btn btn-outline-primary " type="button" onClick={() => console.log('База экспортирована')}>Word</button></a>
               </div>
           </div>
-        </nav>
-  )
+          <div className="py-2">
+            <a href={`${props.API_URL}importBooks`}>
+              <button className="btn btn-outline-success" onClick={() => setToggle({toggle:!toggle, elemID: 'Import'})}>
+                  Импорт базы
+              </button>
+              </a>
+          </div>
+        </nav>  )
 }
